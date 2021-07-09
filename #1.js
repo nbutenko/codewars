33317a611a196c978f1ae5fd71d8e09a4b9767c4
nbutenko <nadea.butenko@gmail.com>
2021-07-08 21:09:39 -0500

    kata #1

---
+++ #1.js
@@ -0,0 +1,12 @@
+function grabDoll(dolls){
+    var bag=[];
+    for (i=0; i<dolls.length; i++){
+        if(dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+            bag.push(dolls[i]);
+        } else {
+            continue;
+        }
+        if(bag.length === 3) break;
+    }
+    return bag;
+}
